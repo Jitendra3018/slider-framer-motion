4c76d295c0251e1ff780b6900d5a1c196cc7dcd1
Jitendra Garg <58606315+Jitendra3018@users.noreply.github.com>
2022-02-02 01:45:54 +0530

    Slider Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { motion } from "framer-motion";
+import { useRef, useState, useEffect } from "react";
+import images from "./images";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [width, setWidth] = useState(0);
+	const carousel = useRef();
+	// console.log(images);
+
+	useEffect(() => {
+		// console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+		setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+	}, []);
+
+	return (
+		<div>
+			{/* Carousal */}
+			<motion.div
+				ref={carousel}
+				className="carousel"
+				whileTap={{ cursor: "grabbing" }}
+			>
+				<motion.h1 className="heading">React Slider</motion.h1>
+				<motion.div
+					drag="x"
+					dragConstraints={{ right: 0, left: -width }}
+					className="innerCarousel"
+				>
+					{images.map((image, index) => (
+						<motion.div className="item" key={index}>
+							<img src={image} alt="" />
+						</motion.div>
+					))}
+				</motion.div>
+			</motion.div>
+		</div>
+	);
 }
 
 export default App;
